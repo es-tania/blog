144f6396b87731e3ad50f9ce998fdf41443f94ee
Tania_es <tane.stvs@gmail.com>
2021-11-22 10:37:24 +0100

    page connexion

---
+++ page_connexion/connexion.php
@@ -0,0 +1,50 @@
+<?php 
+session_start();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="styles_connexion.css">
+</head>
+
+<body>
+
+    <?php	if (isset($_SESSION["login"]))
+{ 
+echo "Bonjour {$_SESSION["login"]}<BR>"; 
+}
+
+
+?>
+
+    <form action="traite_login.php">
+
+        <fieldset>
+            <legend>Connexion</legend>
+
+            <p>Les champs marqués de l'astérisque rouge sont obligatoires.</p>
+            <p>
+                <label class="label" for="pseudo">Pseudo<span class="obligatoire">*</span></label>
+                <input type="text" id="pseudo" name="login" required>
+            </p>
+            <?php 
+	            if (isset($_GET["err"]) && $_GET["err"]=="login") { echo "ATTENTION MAUVAIS LOGIN";}
+		    ?>
+            <p>
+                <label class="label" for="pwd">Mot de passe<span class="obligatoire">*</span></label>
+                <input type="password" id="pwd" name="pwd" required>
+            </p>
+            <?php 
+	            if (isset($_GET["err"]) && $_GET["err"]=="mdp") { echo "ATTENTION MAUVAIS MOT DE PASSE";}
+		    ?>
+            <p class="center">
+                <input type="submit" value="Se connecter">
+            </p>
+        </fieldset>
+    </form>
+
+</body>
+
+</html>
---
+++ page_connexion/traite_login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+$db=new PDO('mysql:host=localhost;dbname=prjt_blog;port=3306;charset=utf8', 'root', '');
+$db->query('SET NAMES utf8');
+
+// requete non préparée :
+//$requete="SELECT * FROM users WHERE login='".$_GET["login"]."'";
+//$stmt=$db->query($requete);
+
+// requete préparée :
+$requete="SELECT * FROM users WHERE login=:login";
+$stmt=$db->prepare($requete);
+$stmt->bindParam(':login',$_GET["login"], PDO::PARAM_STR);
+$stmt->execute();
+
+if ($stmt->rowcount()==1){
+	$result=$stmt->fetch(PDO::FETCH_ASSOC);
+	if (password_verify($_GET["pwd"],$result["passwd"])){
+		echo "SUPER !!! vous etes connecté";
+		$_SESSION["login"]=$_GET["login"];
+		echo '<br><a href="affiche_utilisateurs.php">afficher les utilisateurs</a>';
+
+	} else {
+		// echo $_GET["pwd"];
+		header ('Location:saisie_login.php?err=mdp');
+	}
+
+} else {
+	header ('Location:saisie_login.php?err=login');
+}
